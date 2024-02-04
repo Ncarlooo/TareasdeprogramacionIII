431786a7f695ba0b3ff7715e979181778757f6c9
Carlo Sosa <157521105+Ncarlooo@users.noreply.github.com>
2024-02-04 11:40:05 -0600

    Add files via upload

---
+++ Ciclos y operadores logicos.py
@@ -0,0 +1,11 @@
+#Carlo Sosa 0901-22-1106
+
+#Usando for:
+for i in range(2, 21, 2):  #Se utiliza cuando conoces la cantidad exacta de veces que deseas que se repita el bucle.
+    print(i)
+
+#Usando while
+#i = 2
+#while i <= 20:
+    #print(i)
+    #i += 2
