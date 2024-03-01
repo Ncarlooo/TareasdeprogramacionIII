bbe03da8cc19108121af5373be8ebd2145cbb14c
Carlo Sosa <157521105+Ncarlooo@users.noreply.github.com>
2024-03-01 11:52:13 -0600

    Add files via upload

---
+++ 4parcial.py
@@ -0,0 +1,8 @@
+#carlo Sosa
+titulo= "El diario the gref"
+Autor = " de Santillana"
+Año = "del 2017"
+Genero = "Fantasia"
+
+print("El libro es:",titulo,Autor,Año)
+print("El genero de este libro es: ", Genero)
