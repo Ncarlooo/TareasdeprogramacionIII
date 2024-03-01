76f45a88c92d4a77ef1e89349f445eecd3c99b85
Carlo Sosa <157521105+Ncarlooo@users.noreply.github.com>
2024-03-01 11:45:49 -0600

    Add files via upload

---
+++ 3parcial.py
@@ -0,0 +1,14 @@
+#carlo Sosa 0901-22-1106
+lista1 = float(input("ingresa tu primer numero "))
+lista2 = float(input("ingresa tu segundo numero "))
+lista3 = float(input("ingresa tu tercero numero "))
+lista4 = float(input("ingresa tu cuarto numero "))
+lista = lista1+lista2+lista3+lista4
+
+print("tu lista es sumada es : ", lista)
+
+suma = lista % 2 == 0
+
+print("Si es tru es par y si sale false es impar, Tu resultado es:", suma)
+
+
